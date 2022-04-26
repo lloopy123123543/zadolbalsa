01c6d15d844c73003fe219dc49d642609644cdad
Igor Parfenov <lloopy123123543@gmail.com>
2022-04-26 23:06:06 +0500

    admin

--- roles.js
+++ roles.js
@@ -4,29 +4,29 @@ let text = document.querySelector(".text");
 let bg = document.querySelector(".image");
 document.querySelector(".ch1").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag4.jpg)';
-    text.innerHTML="Это Первая кнопочка";
+    text.innerHTML="Создатель сервера";
 })
 document.querySelector(".ch2").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag3.jpg)';
-    text.innerHTML="Это вторая кнопочка";
+    text.innerHTML="Модеры";
 })
 document.querySelector(".ch3").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag2.png)';
-    text.innerHTML="Это третья кнопочка";
+    text.innerHTML="Подтвержденные ученики";
 })
 document.querySelector(".ch4").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag.png)';
-    text.innerHTML="Это четвертая кнопочка";
+    text.innerHTML="Админы";
 })
 document.querySelector(".ch5").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag.png)';
-    text.innerHTML="Это пятая кнопочка";
+    text.innerHTML="Староста (может кидать в мут людей)";
 })
 document.querySelector(".ch6").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag.png)';
-    text.innerHTML="Это шестая кнопочка";
+    text.innerHTML="Продвинутый ученик";
 })
 document.querySelector(".ch7").addEventListener('click', function(){
     bg.style.background = 'url(./img/bag.png)';
-    text.innerHTML="Это седьмая кнопочка";
+    text.innerHTML="Не подтвержденный ученик";
 })
